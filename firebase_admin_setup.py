ccbf206733bb6115967f8cc0bf65c5aa807f593f
Haifasu <Haifasu@users.noreply.github.com>
2025-11-22 02:48:09 +0300

    h

--- firebase_admin_setup.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("service-account.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
